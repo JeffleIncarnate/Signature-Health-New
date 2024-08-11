7c95c0dbaa3c0ac0b32944910722b7486e87a472
Jeffle Incarnate <dhruvrayat50@gmail.com>
2024-08-11 17:34:22 +1200

    feat: fix links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
 import "./globals.css";
 
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
+
 export default function RootLayout({
   children,
 }: Readonly<{
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,33 +3,35 @@ import "./footer.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin, faFacebook } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer className="Footer">
       <div className="Links">
-        <a href="/about">About Us</a>
+        <Link href="/about">About Us</Link>
         <i>•</i>
-        <a href="/services">Our Services</a>
+        <Link href="/services">Our Services</Link>
         <i>•</i>
-        <a href="/contact">Contact Us</a>
+        <Link href="/contact">Contact Us</Link>
       </div>
+
       <div className="Socials">
-        <a href="/contact">
-          <FontAwesomeIcon icon={faEnvelope} />
-        </a>
+        <Link href={"/contact"}>
+          <FontAwesomeIcon className="Socials__Icon" icon={faEnvelope} />
+        </Link>
 
         <a
           href="https://www.linkedin.com/company/signaturehealthgroup/"
           target="_blank"
         >
-          <FontAwesomeIcon icon={faLinkedin} />
+          <FontAwesomeIcon className="Socials__Icon" icon={faLinkedin} />
         </a>
         <a
           href="https://www.facebook.com/profile.php?id=61563617425142"
           target="_blank"
         >
-          <FontAwesomeIcon icon={faFacebook} />
+          <FontAwesomeIcon className="Socials__Icon" icon={faFacebook} />
         </a>
       </div>
     </footer>
